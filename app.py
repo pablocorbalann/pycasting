2c346a8d0055421d6d165107449e8e1c48c307cd
pablocorbcon <pablocorbcon@gmail.com>
2020-11-21 10:12:15 +0100

    feat: basic raycasting created

--- app.py
+++ app.py
@@ -1,61 +1,76 @@
-import pygame
-import json
-from src.player import Player
-from src.walls import Walls
+# This is the main file for the 2d-raycasting Python project.
+# Check it out at: https://github.com/pablocorbcon/2d-raycast
 
-pygame.init()
-def load_configuration():
-    with open("CONFIG.json") as f:
-        return json.load(f)
+# imports needed
+import numpy as np
+import pygame 
+from ray import *
+from particle import *
 
-def main():
-    # constants
-    WIDTH, HEIGHT = 600, 600
-    SIZE = (WIDTH, HEIGHT)
-    CLOCK = pygame.time.Clock()
-    MAX_FPS = 120
-    # load the configuration of the app
-    CONFIGURATION = load_configuration()
+# Global constants
+WIDTH = 800
+HEIGHT = 800
+SIZE = (WIDTH, HEIGHT)
 
-    # screen configuration
-    screen = pygame.display.set_mode(SIZE)
-    pygame.display.set_caption(CONFIGURATION['general']['title'])
-    # sprites
-    player = Player(CONFIGURATION["player"])
-    walls = Walls(CONFIGURATION["walls"])
-    # game loop of the app
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key in [pygame.K_w, pygame.K_UP]:
-                    player.up_pressed = True
-                elif event.key in [pygame.K_s, pygame.K_DOWN]:
-                    player.down_pressed = True
-                elif event.key in [pygame.K_d, pygame.K_RIGHT]:
-                    player.right_pressed = True
-                elif event.key in [pygame.K_a, pygame.K_LEFT]:
-                    player.left_pressed = True
-            elif event.type == pygame.KEYUP:
-                if event.key in [pygame.K_w, pygame.K_UP]:
-                    player.up_pressed = False
-                elif event.key in [pygame.K_s, pygame.K_DOWN]:
-                    player.down_pressed = False
-                elif event.key in [pygame.K_d, pygame.K_RIGHT]:
-                    player.right_pressed = False
-                elif event.key in [pygame.K_a, pygame.K_LEFT]:
-                    player.left_pressed = False
-        # update
-        screen.fill(CONFIGURATION['general']['color'])
-        player.draw(screen)
-        walls.draw(screen)
-        player.check_borders(SIZE)
-        player.update()
-        pygame.display.flip()
-        CLOCK.tick(MAX_FPS)
+
+class Display:
+    """
+    This class manages the display and visual part of it.
+    """
+    def __init__(self):
+        """
+        This is the constructor method for the Display class. From here all the attributes
+        are created.
+
+        Parameters:
+            self => The Display class
+        """
+        pygame.init()
+        self.screen = pygame.display.set_mode(SCREEN)
+
+        #Random walls
+        self.walls = []
+        for i in range(5):
+            x1 = np.random.randint(0,WIDTH)
+            y1  = np.random.randint(0, HEIGHT)
+            x2 = np.random.randint(0, WIDTH)
+            y2 = np.random.randint(0, HEIGHT)
+            x3 = np.random.randint(0, WIDTH)
+            y3 = np.random.randint(0, HEIGHT)
+            self.walls.append(Limits(x1,y1,x2,y2))
+
+        self.walls.append(Limits(0,0,WIDTH,0))
+        self.walls.append(Limits(0, 0, 0, HEIGHT))
+        self.walls.append(Limits(0, HEIGHT, WIDTH, HEIGHT))
+        self.walls.append(Limits(WIDTH, 0, WIDTH, HEIGHT))
+        self.particle = Particle()
+        self.stopgame = False
+        self.clock = pygame.time.Clock()
+
+    def draw(self):
+        for wall in self.walls:
+            wall.display(self.screen)
+        self.particle.display(self.screen)
+
+
+    def run(self):
+        while not self.stopgame:
+            self.screen.fill((0, 0, 0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.stopgame = True
+                #mouse position
+                if event.type == pygame.MOUSEMOTION:
+                    pos = event.pos
+                    self.particle.pos[0] = pos[0]
+                    self.particle.pos[1] = pos[1]
+
+            self.particle.look(self.screen,self.walls)
+            self.draw()
+            self.clock.tick(100)
+            pygame.display.update()
+
 
 if __name__ == '__main__':
-    main()
+    D = Display()
+    D.run()
